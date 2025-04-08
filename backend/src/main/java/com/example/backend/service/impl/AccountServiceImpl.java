a4da32c6a243439f843c8faf5ddc0de84e782f97
swn233 <swndaze@gmail.com>
2025-04-08 17:05:00 +0800

    用户信息数据库校验

---
+++ backend/src/main/java/com/example/backend/service/impl/AccountServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.backend.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.backend.entity.dto.Account;
+import com.example.backend.mapper.AccountMapper;
+import com.example.backend.service.AccountService;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> implements AccountService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account=this.findAccountByNameOrEmail(username);
+        if (account==null){
+            throw new UsernameNotFoundException("用户名或密码错误");
+        }
+        return User.withUsername(username)
+                .password(account.getPassword())
+                .roles(account.getRole())
+                .build();
+    }
+
+    public Account findAccountByNameOrEmail(String text){
+        return this.query()
+                .eq("username",text).or()
+                .eq("email",text)
+                .one();
+    }
+}
+
